bb4c2c5dee4049f399c89ea9bad6230391acd366
LRoberti99 <116340256+LRoberti99@users.noreply.github.com>
2023-12-13 14:57:10 +0100

    Add files via upload

---
+++ Esercizio_7_2.java
@@ -0,0 +1,25 @@
+package esercizi_academy;
+
+public class Esercizio_7_2 {
+
+    int punteggioA = 3;
+    int punteggioB = 0;
+
+    public static void main(String[] args){
+        int punteggioA = 3;
+        int punteggioB = 3;
+
+        if(punteggioA>punteggioB)
+        {
+            System.out.println("Vince A!");
+        }
+        else if(punteggioA<punteggioB)
+        {
+            System.out.println("Vince B!");
+        }else
+        {
+            System.out.println("Pareggio!");
+        }
+
+    }
+}
